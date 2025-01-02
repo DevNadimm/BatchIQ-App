a6e705d8ec932f23b607b364a69085013c4e3f89
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-02 22:58:58 +0600

    update

--- lib/core/binders/controller_binders.dart
+++ lib/core/binders/controller_binders.dart
@@ -1,5 +1,6 @@
 import 'package:batchiq_app/features/auth/controller/sign_in_controller.dart';
 import 'package:batchiq_app/features/auth/controller/sign_up_controller.dart';
+import 'package:batchiq_app/features/create_batch/controller/apply_admin_controller.dart';
 import 'package:get/get.dart';
 
 class ControllerBinders implements Bindings{
@@ -7,5 +8,6 @@ class ControllerBinders implements Bindings{
   void dependencies() {
     Get.put(SignInController());
     Get.put(SignUpController());
+    Get.put(ApplyAdminController());
   }
 }
