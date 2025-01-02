f2d6d2bc81bdd9ddbf0c62b1e2a315ed9ce2f725
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-02 20:34:40 +0600

    update

---
+++ lib/features/auth/screens/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:batchiq_app/core/colors/colors.dart';
+import 'package:batchiq_app/features/auth/screens/sign_in_screen.dart';
+import 'package:batchiq_app/features/join_batch/screens/join_batch_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome()async {
+    final auth = FirebaseAuth.instance;
+    await Future.delayed(const Duration(seconds: 3));
+    if(auth.currentUser == null){
+      Get.offAll(const SignInScreen());
+    } else {
+      Get.offAll(const JoinBatchScreen());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logos/google.png',
+             scale: 8,
+            ),
+            const SizedBox(height: 20),
+            Text("BatchIQ", style: Theme.of(context).textTheme.headlineLarge!.copyWith(color: Colors.white))
+          ],
+        ),
+      ),
+    );
+  }
+}
