2a53371897e70d6f0abd2ae97409c453c4beca11
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-08 13:42:44 +0600

    update remove prefs

--- lib/features/batch_management/join_batch/widgets/batch_id_section.dart
+++ lib/features/batch_management/join_batch/widgets/batch_id_section.dart
@@ -1,11 +1,20 @@
+import 'package:batchiq_app/core/utils/ui/snackbar_message.dart';
+import 'package:batchiq_app/features/batch_management/join_batch/controller/join_batch_controller.dart';
+import 'package:batchiq_app/features/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:batchiq_app/core/colors/colors.dart';
+import 'package:get/get.dart';
 
 class BatchIDSection extends StatelessWidget {
-  const BatchIDSection({super.key});
+  const BatchIDSection({super.key, required this.userId});
+
+  final String userId;
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController batchId = TextEditingController();
+    final controller = JoinBatchController.instance;
+
     return Container(
       padding: const EdgeInsets.all(4),
       decoration: BoxDecoration(
@@ -14,12 +23,13 @@ class BatchIDSection extends StatelessWidget {
       ),
       child: Row(
         children: [
-          const Expanded(
+          Expanded(
             child: TextField(
-              decoration: InputDecoration(
+              controller: batchId,
+              decoration: const InputDecoration(
                 hintText: "e.g. A7B3D2E1",
-                hintStyle: TextStyle(
-                    fontWeight: FontWeight.w500, color: Colors.grey),
+                hintStyle:
+                    TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
                 contentPadding: EdgeInsets.symmetric(
                   vertical: 0,
                   horizontal: 12,
@@ -32,13 +42,30 @@ class BatchIDSection extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius: BorderRadius.circular(10),
+          GestureDetector(
+            onTap: () async {
+              final result = await controller.joinBatch(
+                  userId: userId, batchId: batchId.text.trim());
+              if (result) {
+                batchId.clear();
+                SnackBarMessage.successMessage(controller.errorMessage ?? "Successfully joined the batch!");
+                // Get.offAll(const HomeScreen());
+              } else {
+                SnackBarMessage.errorMessage(controller.errorMessage ?? "Something went wrong!");
+              }
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: const EdgeInsets.all(12),
+              child: controller.isLoading
+                  ? const CircularProgressIndicator(
+                      color: Colors.white,
+                    )
+                  : const Icon(Icons.arrow_forward_rounded, color: Colors.white),
             ),
-            padding: const EdgeInsets.all(12),
-            child: const Icon(Icons.arrow_forward_rounded, color: Colors.white),
           ),
         ],
       ),
