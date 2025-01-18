f17f4e035d37feabfba26f5a4fa62d7ee13ecb19
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-18 20:48:07 +0600

    update

---
+++ lib/features/admin_dashboard/screens/notification_screens/notification_admin_screen.dart
@@ -0,0 +1,113 @@
+import 'package:batchiq_app/core/utils/ui/empty_list.dart';
+import 'package:batchiq_app/core/constants/icons_name.dart';
+import 'package:batchiq_app/core/utils/ui/progress_indicator.dart';
+import 'package:batchiq_app/features/admin_dashboard/controller/notification_admin_controller.dart';
+import 'package:batchiq_app/features/admin_dashboard/widgets/notification_card.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NotificationAdminScreen extends StatefulWidget {
+  const NotificationAdminScreen({super.key});
+
+  @override
+  State<NotificationAdminScreen> createState() =>
+      _NotificationAdminScreenState();
+}
+
+class _NotificationAdminScreenState extends State<NotificationAdminScreen> {
+  @override
+  void initState() {
+    fetchAnnouncement();
+    super.initState();
+  }
+
+  Future<void> fetchAnnouncement() async {
+    final controller = NotificationAdminController.instance;
+    await controller.getNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        title: Text(
+          "Notification",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: Icon(backArrow),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.5),
+          child: Container(
+            color: Colors.grey.withOpacity(0.2),
+            height: 1.5,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GetBuilder<NotificationAdminController>(
+              builder: (controller) {
+                return Visibility(
+                  visible: !controller.isLoading,
+                  replacement: const ProgressIndicatorWidget(),
+                  child: controller.notifications.isEmpty
+                      ? const EmptyList(
+                          title: "Empty Notification!",
+                        )
+                      : SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 8),
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: controller.notifications.length,
+                                itemBuilder: (context, index) {
+                                  final notification = controller.notifications[index];
+                                  return NotificationCard(
+                                    notification: notification,
+                                  );
+                                },
+                              ),
+                              const SizedBox(height: 8),
+                            ],
+                          ),
+                        ),
+                );
+              },
+            ),
+          ),
+          Column(
+            children: [
+              Divider(
+                height: 1.5,
+                thickness: 1.5,
+                color: Colors.grey.withOpacity(0.2),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Create Notification",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
