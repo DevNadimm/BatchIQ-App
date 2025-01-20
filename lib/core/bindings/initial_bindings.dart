b510839d03341910853bca12b533d9b958900e5c
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-20 12:47:43 +0600

    update

--- lib/core/bindings/initial_bindings.dart
+++ lib/core/bindings/initial_bindings.dart
@@ -1,27 +1,30 @@
-import 'package:batchiq_app/features/admin_dashboard/controller/announcement_controller.dart';
-import 'package:batchiq_app/features/admin_dashboard/controller/assignment_controller.dart';
-import 'package:batchiq_app/features/admin_dashboard/controller/count_members_controller.dart';
-import 'package:batchiq_app/features/admin_dashboard/controller/create_announcement_controller.dart';
-import 'package:batchiq_app/features/admin_dashboard/controller/notification_controller.dart';
+import 'package:get/get.dart';
 import 'package:batchiq_app/features/auth/controller/sign_in_controller.dart';
 import 'package:batchiq_app/features/auth/controller/sign_up_controller.dart';
 import 'package:batchiq_app/features/batch_management/create_batch/controller/apply_admin_controller.dart';
 import 'package:batchiq_app/features/batch_management/create_batch/controller/create_batch_controller.dart';
 import 'package:batchiq_app/features/batch_management/join_batch/controller/join_batch_controller.dart';
-import 'package:get/get.dart';
+import 'package:batchiq_app/features/admin_dashboard/controller/announcement_controller.dart';
+import 'package:batchiq_app/features/admin_dashboard/controller/assignment_controller.dart';
+import 'package:batchiq_app/features/admin_dashboard/controller/count_members_controller.dart';
+import 'package:batchiq_app/features/admin_dashboard/controller/notification_controller.dart';
 
-class InitialBindings implements Bindings{
+class InitialBindings implements Bindings {
   @override
   void dependencies() {
+    // Auth Feature
     Get.put(SignInController());
     Get.put(SignUpController());
+
+    // Batch Management Feature
     Get.put(ApplyAdminController());
     Get.put(CreateBatchController());
     Get.put(JoinBatchController());
-    Get.put(AssignmentController());
+
+    // Admin Dashboard Feature
     Get.put(MemberCountController());
+    Get.put(AssignmentController());
     Get.put(NotificationController());
     Get.put(AnnouncementController());
-    Get.put(CreateAnnouncementController());
   }
 }
