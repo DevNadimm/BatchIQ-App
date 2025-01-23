dfb1d12a9123dc41cf6adbef6c8f1ac622f4e5f4
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-23 15:21:15 +0600

    under_maintenance_screen

---
+++ lib/features/others/screens/under_maintenance_screen.dart
@@ -0,0 +1,98 @@
+import 'package:batchiq_app/features/others/widgets/maintenance_banner.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class UnderMaintenanceScreen extends StatelessWidget {
+  const UnderMaintenanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Spacer(flex: 2),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: SvgPicture.string(
+                    maintenanceBanner,
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+              const Spacer(flex: 2),
+              ErrorInfo(
+                title: "Under Maintenance!",
+                description:
+                "We’re taking some time to make improvements just for you! Please check back shortly, and thank you for your support!",
+                buttonText: "Go Back",
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ErrorInfo extends StatelessWidget {
+  const ErrorInfo({
+    super.key,
+    required this.title,
+    required this.description,
+    required this.buttonText,
+    required this.onTap,
+  });
+
+  final String title;
+  final String description;
+  final String buttonText;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 400),
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              description,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: 16 * 2.5),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: onTap,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(buttonText),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
