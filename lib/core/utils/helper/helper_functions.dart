e2ff51a095c55aaceacba1fb63445f425fb22c74
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-24 21:22:45 +0600

    class schedule update

--- lib/core/utils/helper/helper_functions.dart
+++ lib/core/utils/helper/helper_functions.dart
@@ -3,21 +3,35 @@ import 'package:intl/intl.dart';
 class HelperFunctions {
   HelperFunctions._();
 
-  String getDayName() {
+  static String getDayName() {
     final now = DateTime.now();
     final days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     return days[now.weekday % 7];
   }
 
-  DateTime parseTime(String time) {
-    final dateFormat = DateFormat.jm(); // Format -> 09:00 AM
-    return dateFormat.parse(time);
+  static DateTime parseTime(String time) {
+    final dateFormat = DateFormat('hh:mm a');
+    try {
+      time = time.trim();
+      return dateFormat.parse(time);
+    } catch (e) {
+      throw FormatException("Error parsing time: $time. Please ensure it is in the format 'hh:mm AM/PM'.");
+    }
   }
 
-  String getClassStatus(String startTime, String endTime) {
+  static String getClassStatus(String startTime, String endTime) {
     final now = DateTime.now();
-    final startDateTime = parseTime(startTime);
-    final endDateTime = parseTime(endTime);
+
+    final startDateTime = parseTime(startTime).copyWith(
+      year: now.year,
+      month: now.month,
+      day: now.day,
+    );
+    final endDateTime = parseTime(endTime).copyWith(
+      year: now.year,
+      month: now.month,
+      day: now.day,
+    );
 
     if (now.isAfter(endDateTime)) {
       return 'Finished';
