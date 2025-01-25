dd051d507859dc27fb27e72d5df7948ef4f2fef6
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-25 13:44:09 +0600

    update

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:batchiq_app/core/colors/colors.dart';
-import 'package:batchiq_app/core/utils/ui/progress_indicator.dart';
 import 'package:batchiq_app/core/utils/ui/snackbar_message.dart';
 import 'package:batchiq_app/features/admin_dashboard/controller/class_schedule_controller.dart';
 import 'package:batchiq_app/features/auth/controller/user_controller.dart';
@@ -10,6 +9,7 @@ import 'package:batchiq_app/features/home/widgets/header_section.dart';
 import 'package:batchiq_app/features/home/widgets/navigation_drawer.dart';
 import 'package:batchiq_app/features/home/widgets/todays_timeline.dart';
 import 'package:batchiq_app/features/home/widgets/user_content_grid.dart';
+import 'package:batchiq_app/shimmer/home_screen_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hugeicons/hugeicons.dart';
@@ -28,103 +28,106 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    _initializeData();
+    fetchAll();
   }
 
-  Future<void> _initializeData() async {
+  Future<void> fetchAll() async {
     setState(() => isLoading = true);
 
-    try {
-      final userController = UserController();
-      final fetchedUserModel = await userController.fetchUserData();
-      userModel = fetchedUserModel;
+    await _fetchUserData();
+    await _fetchBatchInfo();
+    await _fetchClassSchedules();
 
-      await Future.wait([
-        if (userModel?.batchId != null)
-          BatchInfoController.instance.fetchBatchInfo(userModel!.batchId!),
-        ClassScheduleController.instance.getClassSchedules(),
-      ]);
-    } catch (e) {
-      SnackBarMessage.errorMessage('Failed to load data!');
-    } finally {
-      setState(() => isLoading = false);
-    }
+    setState(() => isLoading = false);
+  }
+
+  Future<void> _fetchUserData() async {
+    final userController = UserController();
+    final fetchedUserModel = await userController.fetchUserData();
+    setState(() {
+      userModel = fetchedUserModel;
+    });
+  }
+
+  Future<void> _fetchBatchInfo() async {
+    await BatchInfoController.instance.fetchBatchInfo(userModel!.batchId!);
+  }
+
+  Future<void> _fetchClassSchedules() async {
+    await ClassScheduleController.instance.getClassSchedules();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: primaryColor,
-        foregroundColor: Colors.white,
-        title: GetBuilder<BatchInfoController>(
-          builder: (controller) => Text(
-            controller.batchInfoModel.name,
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall!
-                .copyWith(color: Colors.white),
-          ),
-        ),
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              icon: const Icon(HugeIcons.strokeRoundedMenu02),
-            );
-          },
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              if (userModel != null && userModel!.batchId != null) {
-                Get.to(
-                  InviteFriendScreen(batchId: userModel!.batchId!),
-                );
-              } else {
-                SnackBarMessage.errorMessage(
-                  'Oops! We couldn’t load your data. Please try again later.',
-                );
-              }
-            },
-            icon: const Icon(HugeIcons.strokeRoundedUserAdd02),
-          ),
-        ],
-      ),
-      drawer: userModel != null
-          ? BatchIQNavigationDrawer(userModel: userModel!)
-          : const Center(
-              child: Text(
-                  'We’re having trouble loading your information. Please refresh the app.'),
+    return Visibility(
+      visible: !isLoading,
+      replacement: const HomeScreenLoading(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: primaryColor,
+          foregroundColor: Colors.white,
+          title: GetBuilder<BatchInfoController>(
+            builder: (controller) => Text(
+              controller.batchInfoModel.name,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(color: Colors.white),
             ),
-      body: isLoading
-          ? const Center(
-              child: ProgressIndicatorWidget(),
-            )
-          : GetBuilder<ClassScheduleController>(
-              builder: (controller) => Column(
-                children: [
-                  const HeaderSection(height: 85),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 24),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: const Icon(HugeIcons.strokeRoundedMenu02),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {
+                if (userModel != null && userModel!.batchId != null) {
+                  Get.to(
+                    InviteFriendScreen(batchId: userModel!.batchId!),
+                  );
+                } else {
+                  SnackBarMessage.errorMessage(
+                    'Oops! We couldn’t load your data. Please try again later.',
+                  );
+                }
+              },
+              icon: const Icon(HugeIcons.strokeRoundedUserAdd02),
+            ),
+          ],
+        ),
+        drawer: userModel != null
+            ? BatchIQNavigationDrawer(userModel: userModel!)
+            : const Center(
+                child: Text(
+                    'We’re having trouble loading your information. Please refresh the app.'),
+              ),
+        body: Column(
+          children: [
+            const HeaderSection(height: 85),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 24),
+                    GetBuilder<ClassScheduleController>(
+                      builder: (controller) =>
                           TodaysTimeline(controller: controller),
-                          const SizedBox(height: 24),
-                          const UserContentGrid(),
-                          const SizedBox(height: 16),
-                        ],
-                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 24),
+                    const UserContentGrid(),
+                    const SizedBox(height: 16),
+                  ],
+                ),
               ),
             ),
+          ],
+        ),
+      ),
     );
   }
 }
