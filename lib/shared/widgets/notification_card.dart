b653dddc99bb7fabc21d0bdee175c6312582b40c
DevNadimm <nadimchowdhury87@gmail.com>
2025-01-26 10:36:29 +0600

    update

--- lib/shared/widgets/notification_card.dart
+++ lib/shared/widgets/notification_card.dart
@@ -1,5 +1,6 @@
 import 'package:batchiq_app/core/colors/colors.dart';
 import 'package:batchiq_app/core/constants/icon_image_name.dart';
+import 'package:batchiq_app/core/utils/helper/helper_functions.dart';
 import 'package:batchiq_app/core/utils/ui/snackbar_message.dart';
 import 'package:batchiq_app/features/admin_dashboard/controller/notification_controller.dart';
 import 'package:batchiq_app/features/admin_dashboard/models/notification_model.dart';
@@ -22,9 +23,30 @@ class NotificationCard extends StatelessWidget {
       children: [
         ListTile(
           splashColor: primaryColor.withOpacity(0.1),
-          leading: CircleAvatar(
-            backgroundColor: Theme.of(context).primaryColor.withOpacity(0.1),
-            child: Image.asset(getNotificationIcon(notification.type), scale: 22),
+          leading: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              CircleAvatar(
+                backgroundColor: Theme.of(context).primaryColor.withOpacity(0.1),
+                child: Image.asset(
+                  getNotificationIcon(notification.type),
+                  scale: 22,
+                ),
+              ),
+              if (!notification.read)
+                Positioned(
+                  right: -5,
+                  top: -5,
+                  child: Container(
+                    width: 10,
+                    height: 10,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+            ],
           ),
           title: Text(
             notification.title,
@@ -35,14 +57,32 @@ class NotificationCard extends StatelessWidget {
                 .titleMedium!
                 .copyWith(fontWeight: FontWeight.bold),
           ),
-          subtitle: Text(
-            notification.body,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: Theme.of(context)
-                .textTheme
-                .bodySmall!
-                .copyWith(color: secondaryFontColor),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 4),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  notification.body,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall!
+                      .copyWith(color: secondaryFontColor),
+                ),
+                const SizedBox(height: 04),
+                Text(
+                  HelperFunctions.parseTimestamp(notification.createdAt),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall!
+                      .copyWith(color: secondaryFontColor),
+                ),
+              ],
+            ),
           ),
           trailing: isAdmin
               ? PopupMenuButton(
@@ -52,56 +92,60 @@ class NotificationCard extends StatelessWidget {
                   elevation: 4,
                   position: PopupMenuPosition.under,
                   itemBuilder: (BuildContext context) => [
-                        PopupMenuItem(
-                          onTap: () {
-                            // Add your edit notification screen navigation logic
-                          },
-                          child: Row(
-                            children: [
-                              Icon(
-                                HugeIcons.strokeRoundedNoteEdit,
-                                color: secondaryFontColor,
-                                size: 20,
-                              ),
-                              const SizedBox(width: 12),
-                              Text(
-                                "Edit",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleLarge!
-                                    .copyWith(color: secondaryFontColor),
-                              ),
-                            ],
+                    PopupMenuItem(
+                      onTap: () {
+                        // Add your edit notification screen navigation logic
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            HugeIcons.strokeRoundedNoteEdit,
+                            color: secondaryFontColor,
+                            size: 20,
                           ),
-                        ),
-                        PopupMenuItem(
-                          onTap: () async {
-                            await deleteNotification();
-                          },
-                          child: Row(
-                            children: [
-                              Icon(
-                                HugeIcons.strokeRoundedDelete01,
-                                color: secondaryFontColor,
-                                size: 20,
-                              ),
-                              const SizedBox(width: 12),
-                              Text(
-                                "Delete",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .titleLarge!
-                                    .copyWith(color: secondaryFontColor),
-                              ),
-                            ],
+                          const SizedBox(width: 12),
+                          Text(
+                            "Edit",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge!
+                                .copyWith(color: secondaryFontColor),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ),
+                    PopupMenuItem(
+                      onTap: () async {
+                        await deleteNotification();
+                      },
+                      child: Row(
+                        children: [
+                          Icon(
+                            HugeIcons.strokeRoundedDelete01,
+                            color: secondaryFontColor,
+                            size: 20,
+                          ),
+                          const SizedBox(width: 12),
+                          Text(
+                            "Delete",
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge!
+                                .copyWith(color: secondaryFontColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                   child: const Icon(
                     HugeIcons.strokeRoundedMoreVertical,
                     color: Colors.grey,
-                  ))
-              : const Icon(Icons.chevron_right, color: Colors.grey),
+                  ),
+                )
+              : const Icon(
+                  Icons.chevron_right,
+                  color: Colors.grey,
+                ),
           onTap: () {},
           contentPadding:
               const EdgeInsets.symmetric(vertical: 8, horizontal: 16.0),
@@ -116,14 +160,11 @@ class NotificationCard extends StatelessWidget {
 
   Future<void> deleteNotification() async {
     final controller = NotificationController.instance;
-    final isDelete =
-        await controller.deleteNotification(notificationId: notification.id);
+    final isDelete = await controller.deleteNotification(notificationId: notification.id);
     if (isDelete) {
-      SnackBarMessage.successMessage(
-          "Your notification has been deleted successfully!");
+      SnackBarMessage.successMessage("Your notification has been deleted successfully!");
     } else {
-      SnackBarMessage.errorMessage(
-          controller.errorMessage ?? "Something went wrong!");
+      SnackBarMessage.errorMessage(controller.errorMessage ?? "Something went wrong!");
     }
   }
 
